43bd362e52b41efdf67886ad2e4bf79e2f470db2
Diego Duarte <duartqx@gmail.com>
2023-10-12 01:34:00 -0300

    spring start

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/greetings")
+	public String greetings(@RequestParam(value = "name", defaultValue = "World") String name) {
+		String format = String.format("Hello, %s!", name);
+		return format;
+	}
+}
